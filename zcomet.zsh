91228b9dfe0be4bf950bcbb1bbad92860a3e0c05
Alexandros Kozak <git-agkozak@sneakemail.com>
2021-10-04 20:29:15 -0700

    Support for local snippets

--- zcomet.zsh
+++ zcomet.zsh
@@ -409,11 +409,22 @@ _zcomet_fpath_command() {
 _zcomet_snippet_command() {
   [[ -z $1 ]] && print 'You need to specify a snippet.' && return 1
 
-  local update snippet url method snippet_file snippet_dir
+  local update snippet url method snippet_file snippet_dir ret
 
   [[ $1 == '--update' ]] && update=1 && shift
   snippet=$1 && shift
 
+  # Local snippets
+  if [[ $snippet != http(|s)://* && $snippet != OMZ::* ]]; then
+    if [[ -r $snippet ]] && source $snippet; then
+      _zcomet_add_list "$cmd" "${${snippet:a}/${HOME}/~}"
+      return
+    fi
+    >&2 print "Could not source snippet ${snippet}."
+    return 1
+  fi
+
+  # Remote snippets
   _zcomet_snippet_shorthand "$snippet"
   url=$REPLY
   snippet_file=${snippet##*/}
@@ -455,7 +466,9 @@ _zcomet_snippet_command() {
   if source "${ZCOMET[SNIPPETS_DIR]}/${snippet_dir}/${snippet_file}"; then
     _zcomet_add_list "$cmd" "$snippet"
   else
+    ret=$?
     >&2 print "Could not source snippet ${snippet}."
+    return $ret
   fi
 }
 
