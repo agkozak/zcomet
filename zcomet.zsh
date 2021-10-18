98ff223c74ceda260a63cb3082b427d80e63823b
Alexandros Kozak <git-agkozak@sneakemail.com>
2021-10-18 15:35:47 -0700

    Local variable

--- zcomet.zsh
+++ zcomet.zsh
@@ -428,7 +428,7 @@ _zcomet_fpath_command() {
 _zcomet_snippet_command() {
   [[ -z $1 ]] && print 'You need to specify a snippet.' && return 1
 
-  local update snippet url method snippet_file snippet_dir
+  local update snippet url method snippet_file snippet_dir ret
 
   [[ $1 == '--update' ]] && update=1 && shift
   snippet=$1 && shift
