770e820ec7ddda90c5c53b4dce2920f45349a6b8
Alexandros Kozak <git-agkozak@sneakemail.com>
2021-10-23 09:50:53 -0700

    A snippet must be a file

--- zcomet.zsh
+++ zcomet.zsh
@@ -446,7 +446,7 @@ _zcomet_snippet_command() {
 
   # Local snippets
   if [[ $snippet != http(|s)://* && $snippet != OMZ::* ]]; then
-    if [[ -r $snippet ]] && source $snippet; then
+    if [[ -f $snippet ]] && source $snippet; then
       _zcomet_add_list "$cmd" "${${snippet:a}/${HOME}/~}"
       return
     fi
